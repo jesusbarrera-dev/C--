e16cc55147a4bb3803571970a9593aca30fb78df
Jesús Barrera <jesusbarreradev@gmail.com>
2021-03-30 13:36:22 -0600

    clases Lexer, Parser y SyntaxToken realizadas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace C__
 {
@@ -152,38 +153,56 @@ namespace C__
 
     enum SyntaxKind{
         NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        MinusToken,
+        TimesToken,
+        DivisionToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
         FloatToken,
         StringToken,
         OrToken,
         boolToken,
         dotToken,
         LoopToken,
-        ConditionalToken
+        ConditionalToken,
+        BadToken,
+        EOFToken
     };
 
-    class SyntaxToken
+    class SyntaxToken : SyntaxNode
     {
-        public SyntaxToken(SyntaxKind kind, int position, string text)
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
         {
             Kind = kind;
             Position = position;
             Text = text;
         }
 
-        public SyntaxKind Kind { get; }
+        public override SyntaxKind Kind { get; }
         public int Position { get; }
         public string Text { get; }
+        public object Value { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            return Enumerable.Empty<SyntaxNode>();
+        }
     }
 
     class Lexer
     {
         private readonly string _text;
         private int _position;
+        private List<string> _diagnostics = new List<string>();
 
         public Lexer(string text)
         {
             _text = text;
         }
+
+        public IEnumerable<string> Diagnostics => _diagnostics;
 
         private char Current
         {
@@ -197,25 +216,191 @@ namespace C__
             }
         }
 
-        //private void Next()
-        //{
-        //    _position++;
-        //}
+        private void Next()
+        {
+            _position++;
+        }
 
-        //public SyntaxToken nextToken()
-        //{
-        //    if (char.IsDigit(Current))
-        //    {
-        //        var start = _position;
-        //        while (char.IsDigit(Current))
-        //        {
-        //            Next();
-        //        }
-        //
-        //        var lenght = _position - start;
-        //        var text = _text.Substring(start, lenght);
-        //        return new SyntaxToken(SyntaxKind.NumberToken, start, text);
-        //    }
-        //}
+        public SyntaxToken nextToken()
+        {
+
+            if (_position >= _text.Length)
+            {
+                return new SyntaxToken(SyntaxKind.EOFToken, _position, "\0", null);
+            }
+
+            if (char.IsDigit(Current))
+            {
+                var start = _position;
+                while (char.IsDigit(Current))
+                {
+                    Next();
+                }
+
+                var lenght = _position - start;
+                var text = _text.Substring(start, lenght);
+                if(!int.TryParse(text, out var value))
+                {
+                    _diagnostics.Add($"El número {_text} no es Int32 válido.");
+                }
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+
+            if (char.IsWhiteSpace(Current))
+            {
+                var start = _position;
+                while (char.IsWhiteSpace(Current))
+                {
+                    Next();
+                }
+
+                var lenght = _position - start;
+                var text = _text.Substring(start, lenght);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.WhiteSpaceToken, start, text, value);
+            }
+
+            if(Current == '+')
+            {
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+            }
+            else if (Current == '-')
+            {
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            }
+            else if (Current == '*')
+            {
+                return new SyntaxToken(SyntaxKind.TimesToken, _position++, "*", null);
+            }
+            else if (Current == '/')
+            {
+                return new SyntaxToken(SyntaxKind.DivisionToken, _position++, "/", null);
+            }
+            else if (Current == '(')
+            {
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+            }
+            else if (Current == ')')
+            {
+                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+            }
+            _diagnostics.Add($"ERROR: entrada de caracter inválido: '{Current}'");
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
     }
+
+    class Parser
+    {
+        private readonly SyntaxToken[] _tokens;
+        private List<string> _diagnostics = new List<string>();
+        private int _position;
+
+        public Parser(string text)
+        {
+            var tokens = new List<SyntaxToken>();
+
+            var lexer = new Lexer(text);
+            SyntaxToken token;
+            do
+            {
+                token = lexer.nextToken();
+
+                if (token.Kind != SyntaxKind.WhiteSpaceToken &&
+                    token.Kind != SyntaxKind.BadToken)
+                {
+                    tokens.Add(token);
+                }
+            } while (token.Kind != SyntaxKind.EOFToken);
+
+            _tokens = tokens.ToArray();
+            _diagnostics.AddRange(lexer.Diagnostics);
+        }
+
+        public IEnumerable<string> Diagnostics => _diagnostics;
+
+        private SyntaxToken Peek(int offset)
+        {
+            var index = _position + offset;
+            if (index >= _tokens.Length)
+            {
+                return _tokens[_tokens.Length - 1];
+            }
+            return _tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
+
+        private SyntaxToken NextToken(){
+            var current = Current;
+            _position++;
+            return current;
+        }
+
+        private SyntaxToken Match(SyntaxKind kind)
+        {
+            if (Current.Kind == kind)
+            {
+                return NextToken();
+            }
+            _diagnostics.Add($"ERROR: token inesperado <{Current.Kind}>, se esperaba <{kind}>");
+            return new SyntaxToken(kind, Current.Position, null, null);
+        }
+
+        private ExpressionSyntax ParseExpression()
+        {
+            return ParseTerm();
+        }
+
+        public SyntaxTree Parse()
+        {
+            var expression = ParseTerm();
+            var EOFToken = Match(SyntaxKind.EOFToken);
+            return new SyntaxTree(_diagnostics, expression, EOFToken);
+        }
+
+        public ExpressionSyntax ParseTerm()
+        {
+            var left = ParseFactor();
+
+            while (Current.Kind == SyntaxKind.PlusToken ||
+                Current.Kind == SyntaxKind.MinusToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParseFactor();
+                left = new BinaryExpressionSyntax(left, operatorToken, right)
+            }
+
+            return left;
+        }
+
+        private ExpressionSyntax ParseFactor()
+        {
+            var left = ParsePrimaryExpression();
+
+            while (Current.Kind == SyntaxKind.TimesToken ||
+                Current.Kind == SyntaxKind.DivisionToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpressionSyntax(left, operatorToken, right)
+            }
+
+            return left;
+        }
+
+        private ExpressionSyntax ParsePrimaryExpression()
+        {
+            if (Current.Kind == SyntaxKind.OpenParenthesisToken)
+            {
+                var left = NextToken();
+                var expression = ParseExpression();
+                var right = Match(SyntaxKind.CloseParenthesisToken);
+                return new ParenthesizedExpressionSyntax(left, expression, right);
+            }
+            var numberToken = Match(SyntaxKind.NumberToken);
+            return new NumberExpressionSyntax(numberToken);
+        }
+
+    }
+
 }
