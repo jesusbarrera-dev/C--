6ee5ddc705ce47c062a962c92d24b2012acec579
Fatima <fati19lopez@outlookcom>
2021-03-31 22:23:24 -0600

    Se crearon las clases ExpressionSyntax, SyntaxTree y ParenthesizedExpressionSyntax

--- Program.cs
+++ Program.cs
@@ -425,4 +425,49 @@ namespace C__
         }
     }
 
+    abstract class ExpressionSyntax : SyntaxNode
+    {
+    }
+
+    sealed class SyntaxTree
+    {
+        public SyntaxTree(IEnumerable<string> diagnostics, ExpressionSyntax root, SyntaxToken endOfFileToken){
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOfFileToken = endOfFileToken;
+            
+        }
+        
+        public IReadOnlyList<string> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOfFileToken { get; }
+        
+        public static SyntaxTree Parse(string text){
+            var parser = new Parser(text);
+            return parser.Parse();
+        }
+    }
+
+    sealed class ParenthesizedExpressionSyntax : ExpressionSyntax
+    {
+        public ParenthesizedExpressionSyntax(SyntaxToken openParenthesisToken, ExpressionSyntax expression, SyntaxToken closeParenthesisToken){
+            OpenParenthesisToken = openParenthesisToken;
+            Expression = expression;
+            CloseParenthesisToken = closeParenthesisToken;
+        }
+        
+        public override SyntaxKind Kind => SyntaxKind.ParenthesizedExpression;
+        public SyntaxToken OpenParenthesisToken { get; }
+        public ExpressionSyntax Expression { get; }
+        public SyntaxToken CloseParenthesisToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren(){
+            yield return OpenParenthesisToken;
+            yield return Expression;
+            yield return CloseParenthesisToken;            
+        }
+
+    }
+
+
 }
