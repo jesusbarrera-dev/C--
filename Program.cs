00ea81a5b8e0443a3014d4e64314095acebe19a3
crisostoJaqueline <perezcrisostojaqueline@gmail.com>
2021-03-31 23:19:48 -0600

    Se creo la clase Evaluator

--- Program.cs
+++ Program.cs
@@ -367,7 +367,7 @@ namespace C__
             {
                 var operatorToken = NextToken();
                 var right = ParseFactor();
-                left = new BinaryExpressionSyntax(left, operatorToken, right)
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
             }
 
             return left;
@@ -469,5 +469,43 @@ namespace C__
 
     }
 
+    class Evaluator
+    {
+        private readonly ExpressionSyntax _root;
+        public Evaluator(ExpressionSyntax root)
+        {
+            this._root = root;
+        }
+        public int Evaluate()
+        {
+            return EvaluateExpression(_root);
+        }
+        public int EvaluateExpression(BinaryExpressionSyntax node)
+        {
+            if (node is NumberExpressionSyntax n)
+                return (int)n.NumberToken.Value;
 
+            if (node is BinaryExpressionSyntax b)
+            {
+                var left = EvaluateExpression(b.Left);
+                var right = EvaluateExpression(b.Right);
+
+                if (b.OperatorToken.Kind == SyntaxKind.PlusToken)
+                    return left + right;
+                else if (b.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return left - right;
+                else if (b.OperatorToken.Kind == SyntaxKind.StringToken)
+                    return left * right;
+                else if (b.OperatorToken.Kind == SyntaxKind.SlashToken)
+                    return left / right;
+                else
+                    throw new Exception($"Unexpected binary operator {b.OperatorToken.Kind}");
+            }
+
+            if (node is ParenthesizedExpressionSyntax p)
+                return EvaluateExpression(p.Expression);
+
+            throw new Exception($"Unexpected node {node.Kind}");
+        }
+    }
 }
